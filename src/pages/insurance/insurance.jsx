690359023ccc1a83db05ac5c83b0347dc3d49264
Murali <63541419+murali-cse@users.noreply.github.com>
2024-06-02 15:15:07 +0530

    insurance coming soon page done

--- src/pages/insurance/insurance.jsx
+++ src/pages/insurance/insurance.jsx
@@ -1,7 +1,28 @@
+import { Box, Stack } from "@mui/material";
 import React from "react";
 
 const Insurance = () => {
-  return <div>Insurance</div>;
+  return (
+    <>
+      <Stack alignItems={"center"}>
+        <img
+          src="assets/vectors/coming_soon.svg"
+          alt="insurance coming soon"
+          width={"40%"}
+          srcSet="assets/vectors/coming_soon.svg"
+        />
+        <Box alignItems={"center"} mt={8} textAlign={"center"}>
+          <h3 style={{ marginBottom: "10px" }}>
+            Shhh... we're working on something magical!
+          </h3>
+          <h4>
+            We can't wait to share it with you soon. Thanks for your patience.
+            ✨
+          </h4>
+        </Box>
+      </Stack>
+    </>
+  );
 };
 
 export default Insurance;
