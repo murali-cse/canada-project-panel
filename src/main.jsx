fdce3d4cdfcba41e0272f025a737bb91f972c5b2
Murali <63541419+murali-cse@users.noreply.github.com>
2024-06-02 14:02:11 +0530

    redux setup initialized

--- src/main.jsx
+++ src/main.jsx
@@ -8,20 +8,24 @@ import Insurance from "./pages/insurance";
 import Users from "./pages/users";
 import Doctors from "./pages/doctors";
 import Issues from "./pages/issues";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}>
-          <Route element={<Dashboard />} index />
-          <Route path="hospitals" element={<Hospitals />} index />
-          <Route path="insurance" element={<Insurance />} index />
-          <Route path="users" element={<Users />} index />
-          <Route path="doctors" element={<Doctors />} index />
-          <Route path="issues" element={<Issues />} index />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store} >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route element={<Dashboard />} index />
+            <Route path="hospitals" element={<Hospitals />} />
+            <Route path="insurance" element={<Insurance />} />
+            <Route path="users" element={<Users />} />
+            <Route path="doctors" element={<Doctors />} />
+            <Route path="issues" element={<Issues />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
